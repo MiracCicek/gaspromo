9850b9788e1fd2e674da9aa95601de2930373065
Mirac Cicek <mmirac.cicek@gmail.com>
2017-04-02 18:39:46 +0300

    Update mysql.php

--- mysql.php
+++ mysql.php
@@ -3,6 +3,4 @@
 	$baglanti = mysql_connect($mysql_server,$mysql_user,$mysql_password);
 	mysql_select_db($mysql_dbname,$baglanti);
 	print mysql_error();
-	$query = mysql_query("SHOW COLUMNS FROM my_table;");
-	print mysql_error();
 ?>
